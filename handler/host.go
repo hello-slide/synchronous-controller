71d011f14feeaaced349cd6e5fa7b8579bf55bdb
Yuto Watanabe <yuto.w51942@gmail.com>
2021-09-01 17:27:23 +0900

    added handler

---
+++ handler/host.go
@@ -0,0 +1,19 @@
+package handler
+
+import "net/http"
+
+func HostCreateHandler(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func HostAcceptHandler(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func HostResultHandler(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func HostEndHandler(res http.ResponseWriter, req *http.Request) {
+
+}
---
+++ handler/visitor.go
@@ -0,0 +1,11 @@
+package handler
+
+import "net/http"
+
+func VisitorHandler(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func VisitorAnswerHandler(res http.ResponseWriter, req *http.Request) {
+
+}
