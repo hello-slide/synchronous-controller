ca047f387df2fa17ddf341a038c1b055a6efde6b
Yuto Watanabe <yuto.w51942@gmail.com>
2021-09-07 14:50:14 +0900

    added tests

--- database/database_test.go
+++ database/database_test.go
@@ -129,3 +129,84 @@ func TestAnswers(t *testing.T) {
 		t.Fatalf("delete error: %v", err)
 	}
 }
+
+func TestTopic(t *testing.T) {
+	if os.Getenv("LOCAL_TEST") != "db" {
+		return
+	}
+
+	// ----config------
+
+	user := os.Getenv("DB_USER")
+	password := ""
+	dbName := "hello-slide-test"
+	tableName := "topic"
+
+	rootId := "topic_id"
+
+	sampleData := &database.Topic{
+		Id:       rootId,
+		Topic:    "hoge",
+		IsUpdate: false,
+	}
+
+	sampleData2 := &database.Topic{
+		Id:       rootId,
+		Topic:    "huga",
+		IsUpdate: true,
+	}
+
+	// -----end------
+
+	config := database.NewLocalConfig(user, dbName, password)
+
+	dbTopic, err := database.NewDBTopic(tableName, config)
+	if err != nil {
+		t.Fatalf("db connect error: %v", err)
+	}
+
+	if err := dbTopic.CreateTable(); err != nil {
+		t.Fatalf("create table error: %v", err)
+	}
+
+	if err := dbTopic.CreateTopic(sampleData); err != nil {
+		t.Fatalf("create topic error: %v", err)
+	}
+
+	updateFlag, err := dbTopic.GetIsUpdate(rootId)
+	if err != nil {
+		t.Fatalf("get update flag error: %v", err)
+	}
+	if updateFlag {
+		t.Fatal("error: updateflag is not false.")
+	}
+
+	result, err := dbTopic.GetTopic(rootId)
+	if err != nil {
+		t.Fatalf("get topic error: %v", err)
+	}
+	if result != "hoge" {
+		t.Fatalf("the result is different. value: %v", result)
+	}
+
+	// update topic
+	if err := dbTopic.UpdateTopic(sampleData2); err != nil {
+		t.Fatalf("update topic error: %v", err)
+	}
+
+	updateFlag, err = dbTopic.GetIsUpdate(rootId)
+	if err != nil {
+		t.Fatalf("get update flag error: %v", err)
+	}
+	if !updateFlag {
+		t.Fatal("error: updateflag is true.")
+	}
+
+	result, err = dbTopic.GetTopic(rootId)
+	if err != nil {
+		t.Fatalf("get topic error: %v", err)
+	}
+	if result != "huga" {
+		t.Fatalf("the result is different. value: %v", result)
+	}
+}
