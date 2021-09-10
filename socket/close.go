78d1ac3e434bdd6f417d3baec39cdb8c10466384
Yuto Watanabe <yuto.w51942@gmail.com>
2021-09-10 16:58:07 +0900

    changed socket close logic

--- socket/close.go
+++ socket/close.go
@@ -13,26 +13,34 @@ import (
 // Arguments:
 //	ws {*websocket.Conn} - websocket op.
 //	db {*database.DatabaseOp} - database operator.
-//	id {string} - session id.
-func Close(ws *websocket.Conn, db *database.DatabaseOp, id string) {
+//	id {string} - session id or user id.
+func Close(ws *websocket.Conn, db *database.DatabaseOp, status Status, id string) {
 	defer ws.Close()
 
-	topic := database.NewDBTopic(TopicTableName, db)
-	if err := topic.Delete(id); err != nil {
-		logrus.Errorf("The topic could not be deleted. err: %v", err)
-		return
-	}
+	if status == Host {
+		topic := database.NewDBTopic(TopicTableName, db)
+		if err := topic.Delete(id); err != nil {
+			logrus.Errorf("The topic could not be deleted. err: %v", err)
+			return
+		}
 
-	answers := database.NewDBAnswers(AnswersTableName, db)
-	if err := answers.Delete(id); err != nil {
-		logrus.Errorf("The answers could not be deleted. err: %v", err)
-		return
-	}
+		answers := database.NewDBAnswers(AnswersTableName, db)
+		if err := answers.Delete(id); err != nil {
+			logrus.Errorf("The answers could not be deleted. err: %v", err)
+			return
+		}
 
-	connectUsers := database.NewDBConnectUsers(ConnectUsersTablename, db)
-	if err := connectUsers.Delete(id); err != nil {
-		logrus.Errorf("The connect users could not be deleted. err: %v", err)
-		return
+		connectUsers := database.NewDBConnectUsers(ConnectUsersTablename, db)
+		if err := connectUsers.Delete(id); err != nil {
+			logrus.Errorf("The connect users could not be deleted. err: %v", err)
+			return
+		}
+	} else if status == Visitor {
+		connectUser := database.NewDBConnectUsers(ConnectUsersTablename, db)
+		if err := connectUser.DeleteUser(id); err != nil {
+			logrus.Errorf("The connect users could not be deleted. err: %v", err)
+			return
+		}
 	}
 
 	db.Close()
