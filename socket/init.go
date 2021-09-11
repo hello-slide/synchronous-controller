4e2f8f272d8a962fd7e3baabd8d2eb4ad562fdaf
Yuto Watanabe <yuto.w51942@gmail.com>
2021-09-11 13:18:07 +0900

    changed socket initialize logic

--- socket/init.go
+++ socket/init.go
@@ -2,6 +2,7 @@ package socket
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/google/uuid"
 	"github.com/hello-slide/synchronous-controller/database"
@@ -17,21 +18,21 @@ const (
 	Visitor        = iota
 )
 
-// Initialize websocket connection.
-//
-// Assign an id on the Host.
-//
-// Arguments:
-//	we {*websocket.Conn} - websocket conn.
-//	status {Status} - Type to initialize. host or visitor.
-//	db {*database.DatabaseOp} - database op instance.
-//	userId {string} - user id. use only visitor.
-//
-// Returns:
-//	{string} - unique id.
-func Init(ws *websocket.Conn, status Status, db *database.DatabaseOp, userId string) (string, error) {
+type InitializeSocket struct {
+	ws *websocket.Conn
+	db *database.DatabaseOp
+}
+
+func NewInitSocket(ws *websocket.Conn, db *database.DatabaseOp) *InitializeSocket {
+	return &InitializeSocket{
+		ws: ws,
+		db: db,
+	}
+}
+
+func (c *InitializeSocket) Host() (string, error) {
 	var responseMessage map[string]string
-	if err := websocket.JSON.Receive(ws, &responseMessage); err != nil {
+	if err := websocket.JSON.Receive(c.ws, &responseMessage); err != nil {
 		return "", err
 	}
 
@@ -41,76 +42,111 @@ func Init(ws *websocket.Conn, status Status, db *database.DatabaseOp, userId str
 	if !ok {
 		return "", errors.New("you need to specify the type")
 	}
+	if responseType != "0" {
+		return "", errors.New("the initial type must be 0")
+	}
 
-	connectUser := database.NewDBConnectUsers(ConnectUsersTablename, db)
+	connectUser := database.NewDBConnectUsers(ConnectUsersTablename, c.db)
 
-	if responseType == "0" && status == Host {
-		// host
-		uuidObj, err := uuid.NewUUID()
-		if err != nil {
-			return "", err
-		}
-		token := util.NewDateSeed().AddSeed(uuidObj.String())
-		id := token.CreateSpecifyLength(5)
+	// host
+	id, err := c.createId("")
+	if err != nil {
+		return "", err
+	}
 
-		// Databas operation
-		answers := database.NewDBAnswers(AnswersTableName, db)
-		if err := answers.CreateTable(); err != nil {
-			return "", err
-		}
-		if err := connectUser.CreateTable(); err != nil {
-			return "", err
-		}
+	// Databas operation
+	answers := database.NewDBAnswers(AnswersTableName, c.db)
+	if err := answers.CreateTable(); err != nil {
+		return "", err
+	}
+	if err := connectUser.CreateTable(); err != nil {
+		return "", err
+	}
 
-		topicOp := database.NewDBTopic(TopicTableName, db)
-		if err := topicOp.CreateTable(); err != nil {
-			return "", err
-		}
-		topic := &database.Topic{
-			Id:       id,
-			Topic:    "",
-			IsUpdate: false,
-		}
-		if err := topicOp.CreateTopic(topic); err != nil {
-			return "", err
-		}
+	topicOp := database.NewDBTopic(TopicTableName, c.db)
+	if err := topicOp.CreateTable(); err != nil {
+		return "", err
+	}
+	topic := &database.Topic{
+		Id:       id,
+		Topic:    "",
+		IsUpdate: false,
+	}
+	if err := topicOp.CreateTopic(topic); err != nil {
+		return "", err
+	}
 
-		initializeSendMessage := map[string]string{
-			"type":    "0",
-			"version": "1.0",
-			"id":      id,
-		}
+	initializeSendMessage := map[string]string{
+		"type":    "0",
+		"version": "1.0",
+		"id":      id,
+	}
 
-		if err := websocket.JSON.Send(ws, initializeSendMessage); err != nil {
-			return "", err
-		}
+	if err := websocket.JSON.Send(c.ws, initializeSendMessage); err != nil {
+		return "", err
+	}
 
+	return id, nil
+}
+
+func (c *InitializeSocket) Visitor() (string, string, error) {
+	var responseMessage map[string]string
+	if err := websocket.JSON.Receive(c.ws, &responseMessage); err != nil {
+		return "", "", err
+	}
+
+	logrus.Debugf("Init response data: %v", responseMessage)
+
+	responseType, ok := responseMessage["type"]
+	if !ok {
+		return "", "", errors.New("you need to specify the type")
+	}
+	if responseType != "1" {
+		return "", "", errors.New("the initial type must be 1")
+	}
+
+	connectUser := database.NewDBConnectUsers(ConnectUsersTablename, c.db)
+
+	id, ok := responseMessage["id"]
+	if !ok {
+		return "", "", errors.New("id is not found")
+	}
+
+	userId, err := c.createId(id)
+	if err != nil {
+		return "", "", err
+	}
+
+	if err := connectUser.AddUser(&database.ConnectUser{
+		Id:     id,
+		UserId: userId,
+	}); err != nil {
+		return "", "", err
+	}
+
+	// visitor
+	initializeSendMessage := map[string]string{
+		"type":    "1",
+		"version": "1.0",
+	}
+
+	if err := websocket.JSON.Send(c.ws, initializeSendMessage); err != nil {
+		return "", "", err
+	}
+
+	return id, userId, nil
+}
+
+func (c *InitializeSocket) createId(prefix string) (string, error) {
+	uuidObj, err := uuid.NewUUID()
+	if err != nil {
+		return "", err
+	}
+	id := util.NewDateSeed().AddSeed(uuidObj.String()).CreateSpecifyLength(5)
+
+	if len(prefix) == 0 {
 		return id, nil
 	}
-	if responseType == "1" && status == Visitor {
-		id, ok := responseMessage["id"]
-		if !ok {
-			return "", errors.New("id is not found")
-		}
 
-		if err := connectUser.AddUser(&database.ConnectUser{
-			Id:     id,
-			UserId: userId,
-		}); err != nil {
-			return "", err
-		}
-
-		// visitor
-		initializeSendMessage := map[string]string{
-			"type":    "1",
-			"version": "1.0",
-		}
-
-		if err := websocket.JSON.Send(ws, initializeSendMessage); err != nil {
-			return id, err
-		}
-
-		return "", nil
-	}
-	return "", errors.New("the initial type must be 0 or 1")
+	return strings.Join([]string{id, prefix}, ""), nil
 }
