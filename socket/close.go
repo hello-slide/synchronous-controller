bc7b4c9bae7f6cf2a78794caa56fd2d70bcb1fca
Yuto Watanabe <yuto.w51942@gmail.com>
2021-09-11 13:18:22 +0900

    changed docket close logic

--- socket/close.go
+++ socket/close.go
@@ -6,41 +6,58 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-// Close operation.
-//
-// close the all database connection and websocket.
-//
-// Arguments:
-//	ws {*websocket.Conn} - websocket op.
-//	db {*database.DatabaseOp} - database operator.
-//	id {string} - session id or user id.
-func Close(ws *websocket.Conn, db *database.DatabaseOp, status Status, id string) {
-	defer ws.Close()
-	logrus.Debugf("close websocket clients.")
+type CloseSocket struct {
+	ws *websocket.Conn
+	db *database.DatabaseOp
+	id string
+}
 
-	if status == Host {
-		topic := database.NewDBTopic(TopicTableName, db)
-		if err := topic.Delete(id); err != nil {
-			logrus.Errorf("The topic could not be deleted. err: %v", err)
-			return
-		}
-
-		answers := database.NewDBAnswers(AnswersTableName, db)
-		if err := answers.Delete(id); err != nil {
-			logrus.Errorf("The answers could not be deleted. err: %v", err)
-			return
-		}
-
-		connectUsers := database.NewDBConnectUsers(ConnectUsersTablename, db)
-		if err := connectUsers.Delete(id); err != nil {
-			logrus.Errorf("The connect users could not be deleted. err: %v", err)
-			return
-		}
-	} else if status == Visitor {
-		connectUser := database.NewDBConnectUsers(ConnectUsersTablename, db)
-		if err := connectUser.DeleteUser(id); err != nil {
-			logrus.Errorf("The connect users could not be deleted. err: %v", err)
-			return
-		}
+func NewCloseSocket(ws *websocket.Conn, db *database.DatabaseOp, id string) *CloseSocket {
+	return &CloseSocket{
+		ws: ws,
+		db: db,
+		id: id,
 	}
+}
+
+func (c *CloseSocket) HostNoErr() {
+	if err := c.Host(); err != nil {
+		logrus.Infof("ERR socket close: %v", err)
+	}
+}
+
+func (c *CloseSocket) VisitorNoErr() {
+	if err := c.Visitor(); err != nil {
+		logrus.Infof("ERR socket close: %v", err)
+	}
+}
+
+func (c *CloseSocket) Host() error {
+	defer c.ws.Close()
+
+	topic := database.NewDBTopic(TopicTableName, c.db)
+	if err := topic.Delete(c.id); err != nil {
+		return err
+	}
+
+	answers := database.NewDBAnswers(AnswersTableName, c.db)
+	if err := answers.Delete(c.id); err != nil {
+		return err
+	}
+
+	connectUsers := database.NewDBConnectUsers(ConnectUsersTablename, c.db)
+	if err := connectUsers.Delete(c.id); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *CloseSocket) Visitor() error {
+	defer c.ws.Close()
+
+	connectUser := database.NewDBConnectUsers(ConnectUsersTablename, c.db)
+	if err := connectUser.DeleteUser(c.id); err != nil {
+		return err
+	}
+	return nil
 }
